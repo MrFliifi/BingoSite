44530b87fc8abc7955cf906e1e1d041fa5da6dbf
MrFliifi <felix-behrens@hotmail.de>
2025-05-27 19:52:38 +0200

    files are now readable with utf8

--- backend/fileHandling/fileHandler.js
+++ backend/fileHandling/fileHandler.js
@@ -1,26 +1,12 @@
 const fs = require('fs');
 const path = require('path');
 
+
 class fileHandler{
     // needed to acces files in any way
     constructor (filePath, fileName) {
         this.filePath = filePath;
         this.fileName = fileName;
-    }
-
-    /* TODO:
-        Methods to be used in frontend maybe?
-        We need a way to acces file path and name from user
-        Probably need a similiar method in frontend, that 
-        passes that data to backend
-        unsure if constructore doesn't make them obsolete?
-    */
-    setFilePath(userInput){
-        this.filePath = userInput;
-    }
-
-    setFileName(userInput){
-        this.fileName = userInput;
     }
     
     // function apparently has to be async because .close() could be executed
@@ -28,18 +14,42 @@ class fileHandler{
     async createSaveFile() {
         // Sets filepath, filename and file format using path class
         const localFilePath = path.join(this.filePath, `${this.fileName}.json`);
-        
         try {
             // creates file at specified location in write mode
-            const fd = await fs.promises.open(localFilePath, 'w');
+            const creater = await fs.promises.open(localFilePath);
             console.log(`File created at: ${localFilePath}`);
             // frees memory from filesystem
-            await fd.close(); 
+            await creater.close(); 
         } catch (err) {
             console.error("Error creating file:", err);
         }
     }
+
+   /* async writeToSaveFile(challenges){
+        const localFilePath = path.join(this.filePath, `${this.fileName}.json`);
+        try {
+            const writer = await fs.promises.open(localFilePath, 'w');
+            writer.writeFile(challenges);
+            writer.close();
+        } catch (err) {
+            console.error("Error writing file:", localFilePath, err);
+        }
+    }
+    */
+    async readFromSaveFile(){
+        const localFilePath = path.join(this.filePath, `${this.fileName}.json`);
+        try {
+            const reader = await fs.promises.open(localFilePath, 'r');
+            const content = await reader.readFile("utf8");
+            console.log(content);
+            reader.close();
+            return content;
+        } catch (err) {
+            console.error("Error reading file at:", localFilePath, err);
+        }
+    }
 }
 // exampte for how to use the class. also for testing purposes
-const fl = new fileHandler("../backend/fileHandling/", "testFile")
+const fl = new fileHandler("../fileHandling/saveFileLocation", "testFile");
 fl.createSaveFile();
+fl.readFromSaveFile();
