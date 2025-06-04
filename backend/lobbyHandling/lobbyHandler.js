06135e44ba15b367f58f3d6b47306326257051f3
MrFliifi <felix-behrens@hotmail.de>
2025-06-04 16:21:21 +0200

    completed lobbyHandler class

--- backend/lobbyHandling/lobbyHandler.js
+++ backend/lobbyHandling/lobbyHandler.js
@@ -1,27 +1,62 @@
+const {fileHandler} = require("../fileHandling/fileHandler.js");
+
 class lobbyHandler{
 
     constructor (gameMode) {
         this.gameMode = gameMode;
-        this.bingoChallenges = new Array[25];
-        this.bingoColor = new Array[25];
+        this.bingoChallenges = new Array(25);
+        this.bingoColor = new Array(25);
         this.player = [];
     }
 
     // TODO: write function that loops each second that updates the socket
     // setters for all but gameMode
 
-    setBingoChallenges (){
-        this.bingoChallenges = x;
+    getGameMode() {
+        return this.gameMode;
     }
 
-    setBingoColor (i, color) {
+    async setBingoChallenges(fileName){
+        const fileHandlerInst = new fileHandler("../fileHandling/saveFileLocation", fileName);
+        // fill array with data from file
+        let contentArr = await fileHandlerInst.readFromSaveFile();
+
+        // shuffle content of array around
+        for (let i = contentArr.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [contentArr[i], contentArr[j]] = [contentArr[j], contentArr[i]];
+        }
+        // assing challenges to the board
+        for (let i = 0; i < 25; i++){
+            this.bingoChallenges[i] = contentArr[i];
+        }
+    }
+
+    getBingoChallenge() {
+        return this.bingoChallenges;
+    }
+
+    setBingoColor(i, color) {
         this.bingoColor[i] = color;
     }
 
-    setPlayer () {
-        this.player = yield;
+    getBingoColor() {
+        return this.bingoColor;
     }
 
-    
+    setPlayer(playerObj) {
+        this.player.push(playerObj);
+    }
 
+    getPlayer() {
+        return this.player;
+    }
 }
+
+// for testing purposes
+const handler = new lobbyHandler("mode");
+
+(async () => {
+    await handler.setBingoChallenges("testFile");
+    console.log(handler.bingoChallenges); // Note: use handler, not this
+})();
