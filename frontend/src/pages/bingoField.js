b012713f9d0befc5a5ff39ab18f4b303e76de75b
Kevin <kevin.hoeschel93@outlook.de>
2025-07-12 13:16:09 +0200

    -Sending lobby.id to the server via sendPlayerColor

--- frontend/src/pages/bingoField.js
+++ frontend/src/pages/bingoField.js
@@ -82,7 +82,7 @@ function BingoPage() {
     setBingoColors(newColors);
 
     //Sending the Server all Data from the Buttonpress
-    socket.emit("ChallengeField", { colorIndex: index, socketId : socket.id, lobbyId: lobbyId });
+    socket.emit("ChallengeField", { colorIndex: index, socketId : socket.id, lobbyId });
   };
 
   function sendPlayerColor()
@@ -95,7 +95,7 @@ function BingoPage() {
       console.log("Send Data:  " );
       console.log(playerColor);
       
-    socket.emit("sendPlayerColor",{playerColor: playerColor, socketId: socket.id,  } );
+    socket.emit("sendPlayerColor",{playerColor, socketId :socket.id, lobbyId  } );
     }
   }
 
