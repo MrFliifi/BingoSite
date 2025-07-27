98e71f9367f34b08db9c5d1754881bce25939791
MrFliifi <felix-behrens@hotmail.de>
2025-07-27 16:44:20 +0200

    changes to server config

--- backend/server/config.js
+++ backend/server/config.js
@@ -1,12 +1,14 @@
 //variables for the backend
-const domain = "https://bingo-app-cv3k.onrender.com";
-const frontPort = "wss://bingo-app-cv3k.onrender.com";
+const domain = "bingo-app-cv3k.onrender.com";
+const frontPort = process.env.PORT || 3000;
 const backPort = process.env.PORT || 4000;
-
-
+const frontAdress = `wss//${domain}:${frontPort}`;
+const backAdress = `https//${domain}`;
 
 module.exports = {
   domain,
+  frontPort,
+  backPort,
   frontAdress,
-  backAdress,
+  backAdress
 };
